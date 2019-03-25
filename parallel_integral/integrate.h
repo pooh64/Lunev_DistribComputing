ca519512f4dde078e55a84909e77254be1fbaa96
Pukhov Vsevolod <pukhov.vyu@phystech.edu>
2019-03-25 03:11:02 +0300

    parallel_integrate: vtune debug

--- parallel_integral/integrate.h
+++ parallel_integral/integrate.h
@@ -1,7 +1,20 @@
 #ifndef INTEGRATE_H_
 #define INTEGRATE_H_
 
- #define ENABLE_DUMP_LOG
+#define ENABLE_DUMP_LOG
+
+/* Tired looking for error, 128 is enough here */
+#define CACHE_LINE_ALIGN 1024
+
+/* Switching to long double makes 3x faster calculations, but some L1 bounds */
+typedef double worker_tmp_t;
+
+/* #define INTEGRATE_FUNC(x) (2 / (x * x + 1)) */
+#define INTEGRATE_FUNC(x) (x)
+
+#define INTEGRATE_UDP_PORT 4010
+#define INTEGRATE_TCP_PORT 4011
+
 
 #ifdef ENABLE_DUMP_LOG
 #define DUMP_LOG(arg) arg
@@ -13,18 +26,6 @@
 #include <stdio.h>
 
 #define TRACE_LINE (fprintf(stderr, "line: %d\n", __LINE__))
-
-/* Tired looking for error, 128 is enough here */
-#define CACHE_LINE_ALIGN 1024
-
-/* Switch from long double to double removes L1 missrate */
-typedef double worker_tmp_t;
-
-/* #define INTEGRATE_FUNC(x) (2 / (x * x + 1)) */
-#define INTEGRATE_FUNC(x) (x)
-
-#define INTEGRATE_UDP_PORT 4010
-#define INTEGRATE_TCP_PORT 4011
 
 int integrate_multicore(cpu_set_t *cpuset, size_t n_steps,
 	long double base, long double step, long double *result);
